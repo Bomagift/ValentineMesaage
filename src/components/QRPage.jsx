4d86989224ee731fc8d97bc058d26eb37974430b
boma <94929211+Bomagift@users.noreply.github.com>
2025-02-08 08:25:38 +0100

    did image

--- src/components/QRPage.jsx
+++ src/components/QRPage.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import Image1 from "../assets/Images/5df10585-0d21-4e27-8751-250a09ab28bf.jpeg"
+import Image2 from "../assets/Images/a8ea58b1-7eed-4b52-8588-a88451df2e19.jpeg"
+import Image3 from "../assets/Images/b3ecdd42-d776-4f26-9b23-1a9eacfc52a1.jpeg"
+import Image4 from "../assets/Images/IMG_0865.jpeg"
+import Image5 from "../assets/Images/IMG_0867.jpeg"
+import Image6 from "../assets/Images/IMG_0868.jpeg"
+import Image7 from "../assets/Images/IMG_0869.jpeg"
+
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion"; // Import Framer Motion
 import "./QRPage.css";
@@ -17,7 +25,7 @@ const QRPage = () => {
       
       <div className="image-gallery">
         <motion.img
-          src="src\assets\Images\5df10585-0d21-4e27-8751-250a09ab28bf.jpeg"
+          src={Image1}
           alt="Us Together"
           className="qr-image"
           initial={{ opacity: 0, scale: 0.8 }}
@@ -26,7 +34,7 @@ const QRPage = () => {
           whileHover={{ scale: 1.1 }}
         />
         <motion.img
-          src="src/assets/Images/a8ea58b1-7eed-4b52-8588-a88451df2e19.jpeg"
+            src={Image2}
           alt="Another Moment"
           className="qr-image"
           initial={{ opacity: 0, scale: 0.8 }}
@@ -36,7 +44,7 @@ const QRPage = () => {
         />
 
 <motion.img
-          src="src/assets/Images/b3ecdd42-d776-4f26-9b23-1a9eacfc52a1.jpeg"
+            src={Image3}
           alt="Another Moment"
           className="qr-image"
           initial={{ opacity: 0, scale: 0.8 }}
@@ -48,7 +56,7 @@ const QRPage = () => {
 
 
 <motion.img
-          src="src/assets/Images/IMG_0865.jpeg"
+            src={Image4}
           alt="Another Moment"
           className="qr-image"
           initial={{ opacity: 0, scale: 0.8 }}
@@ -58,7 +66,7 @@ const QRPage = () => {
         />
 
 <motion.img
-          src="src/assets/Images/IMG_0867.jpeg"
+          src={Image5}
           alt="Another Moment"
           className="qr-image"
           initial={{ opacity: 0, scale: 0.8 }}
@@ -68,7 +76,7 @@ const QRPage = () => {
         />
 
 <motion.img
-          src="src/assets/Images/IMG_0868.jpeg"
+            src={Image6}
           alt="Another Moment"
           className="qr-image"
           initial={{ opacity: 0, scale: 0.8 }}
@@ -78,7 +86,7 @@ const QRPage = () => {
         />
 
 <motion.img
-          src="src/assets/Images/IMG_0869.jpeg"
+            src={Image6}
           alt="Another Moment"
           className="qr-image"
           initial={{ opacity: 0, scale: 0.8 }}
